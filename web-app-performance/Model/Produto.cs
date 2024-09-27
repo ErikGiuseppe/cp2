83e5f44ba23da5e899d2dc2434005b7ed1a07bda
ErikGiuseppe <erikkato2004@gmail.com>
2024-09-26 22:24:14 -0300

    atualizando

--- web-app-performance/Model/Produto.cs
+++ web-app-performance/Model/Produto.cs
@@ -1,9 +1,17 @@
-﻿namespace web_app_performance.Model
+﻿using System.Data;
+
+namespace web_app_performance.Model
 {
-    public class Usuario
+    public class Produto
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string Email { get; set; }
+        public string Preco { get; set; }
+
+        public double QuantidadeEstoque { get; set; } 
+
+        public DateTime DataCriacao { get; set; }  
+
+
     }
 }
